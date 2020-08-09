b01375f1eb5b59a50993ed1aa9f31b740e92ee49
Romulo Pransteter da Silva <r.pransteter@gmail.com>
2020-08-08 23:50:56 -0300

    Structure - Implement ESlint #4

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'env': {
+    'es6': true,
+    'node': true,
+    'jest': true,
+  },
+  'extends': ['eslint:recommended', 'google'],
+  'ignorePatterns': [
+    '**/node_modules/**',
+    '**/coverage/**',
+  ],
+  'rules': {
+  },
+};
